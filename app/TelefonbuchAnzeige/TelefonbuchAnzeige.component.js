cf13ae9c446a9d0813e897a1fadf6bd5fa0cd132
Martin <martin@example.com>
2017-04-24 11:54:43 +0200

    + Bearbeiten von Einträgen + Löschen von Einträgen + Sortieren nach Nachname,Vorname

--- app/TelefonbuchAnzeige/TelefonbuchAnzeige.component.js
+++ app/TelefonbuchAnzeige/TelefonbuchAnzeige.component.js
@@ -6,23 +6,17 @@ angular.
 		
 			Vornamen = ["Anja", "Bernhard", "Max", "Mara", "Thorsten", "Sabine"];
 			Nachnamen = ["Schmidt", "Müller", "Mayer", "Leineweber"];
-			this.Kontakte = Array(Vornamen.length * Nachnamen.length + 1);
-			this.Kontakte[0] = {
-						Index: 0,
-						BeingEdited: false,
-						Dummy: true
-			};
+			this.Kontakte = Array(Vornamen.length * Nachnamen.length);
 			for (i = 0; i < Vornamen.length; i++) {
 				for (j = 0; j < Nachnamen.length; j++) {
 					var zahl = 37*(7**3+i)*(9**1+j);
 					var t = zahl % 65536;
 					var f = (3*zahl) % 65536;
-					var index = 1 + i*Nachnamen.length + j;
+					var index = i*Nachnamen.length + j;
 					this.Kontakte[index] = {
 						Index: index,
-						BeingEdited: false,
-						IsDummy: false,
-						Name: Vornamen[i] + " " + Nachnamen[j], 
+						Vorname: Vornamen[i],
+						Nachname: Nachnamen[j],
 						Email: Vornamen[i] + "." + Nachnamen[j] + "@example.com",
 						Tel1: "0551 / " + t,
 						Tel2: "0176 / " + f,
@@ -30,7 +24,56 @@ angular.
 					};
 				}
 			}
-			this.Kontakte[0].BeingEdited = true;
+			this.ConfirmDelete = false;
+			this.EditedIndex = -1;
+			this.EditedEntry = {};
+			var Self = this;
+			this.CopyEntry = function(Target, Source) {
+				// Da es sich nur um wenige Felder handelt, mache ich das hier per Hand.
+				Target.Vorname = Source.Vorname;
+				Target.Nachname = Source.Nachname;
+				Target.Email = Source.Email;
+				Target.Addresse = Source.Addresse;
+				Target.Tel1 = Source.Tel1;
+				Target.Tel2 = Source.Tel2;
+			}
+			this.StartEdit = function (Index) {
+				if (Self.EditedIndex == -1 && Index >= 0 && Index < Self.Kontakte.length) {
+					Self.CopyEntry(Self.EditedEntry, Self.Kontakte[Index]);
+					Self.EditedIndex = Index;
+				}
+			};
+			this.CancelEdit = function () {
+				if (Self.EditedIndex != -1) {
+					Self.EditedIndex = -1;
+				}
+			};
+			this.SaveEdit = function () {
+				if (Self.EditedIndex != -1) {
+					Self.CopyEntry(Self.Kontakte[Self.EditedIndex], Self.EditedEntry);
+					Self.EditedIndex = -1;
+				}
+			};
+			this.DeleteEntry1 = function () {
+				if (Self.EditedIndex != -1) {
+					Self.ConfirmDelete = true;
+				}
+			};
+			this.DeleteEntry2 = function (DoDelete) {
+				if (DoDelete && Self.EditedIndex != -1) {
+					// Es muss sichergestellt werden, dass die Indizierung weiterhin korrekt ist.
+					// Da AngularJS die Sortierung übernimmt, genügt es den zu löschenden Eintrag
+					// durch den letzten Eintrag zu ersetzen, nur dessen Index zu aktualisieren
+					// und das Array Kontakte um eins zu verkürzen.
+					if (Self.EditedIndex < Self.Kontakte.length - 1) {
+						Self.Kontakte[Self.EditedIndex] = Self.Kontakte[Self.Kontakte.length - 1];
+						Self.Kontakte[Self.EditedIndex].Index = Self.EditedIndex;
+					}
+					Self.Kontakte.splice(Self.Kontakte.length - 1);
+					Self.EditedIndex = -1;
+				}
+				Self.ConfirmDelete = false;
+			};
 		}
 	});
 
