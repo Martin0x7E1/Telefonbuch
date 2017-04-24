79d1a9bab7b03a51635bf96127c3fd409ecf48e0
Martin <martin@example.com>
2017-04-24 11:01:40 +0200

    + Bearbeiten Maske

--- app/TelefonbuchAnzeige/TelefonbuchAnzeige.component.js
+++ app/TelefonbuchAnzeige/TelefonbuchAnzeige.component.js
@@ -6,13 +6,22 @@ angular.
 		
 			Vornamen = ["Anja", "Bernhard", "Max", "Mara", "Thorsten", "Sabine"];
 			Nachnamen = ["Schmidt", "Müller", "Mayer", "Leineweber"];
-			this.Kontakte = Array(Vornamen.length * Nachnamen.length);
+			this.Kontakte = Array(Vornamen.length * Nachnamen.length + 1);
+			this.Kontakte[0] = {
+						Index: 0,
+						BeingEdited: false,
+						Dummy: true
+			};
 			for (i = 0; i < Vornamen.length; i++) {
 				for (j = 0; j < Nachnamen.length; j++) {
 					var zahl = 37*(7**3+i)*(9**1+j);
 					var t = zahl % 65536;
 					var f = (3*zahl) % 65536;
-					this.Kontakte[i*Nachnamen.length + j] = {
+					var index = 1 + i*Nachnamen.length + j;
+					this.Kontakte[index] = {
+						Index: index,
+						BeingEdited: false,
+						IsDummy: false,
 						Name: Vornamen[i] + " " + Nachnamen[j], 
 						Email: Vornamen[i] + "." + Nachnamen[j] + "@example.com",
 						Tel1: "0551 / " + t,
@@ -21,6 +30,7 @@ angular.
 					};
 				}
 			}
+			this.Kontakte[0].BeingEdited = true;
 		}
 	});
 
