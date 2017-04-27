3cc397fdef62b457c164d9c223f3ad29fe490cbe
Martin <martin@example.com>
2017-04-27 09:26:13 +0200

    + Layout

--- app/TelefonbuchAnzeige/TelefonbuchAnzeige.spec.js
+++
@@ -1,8 +0,0 @@
-describe("telefonbuchAnzeige", function() {
-	beforeEach(module("telefonbuchAnzeige"));
-	
-	it("creates 24 entries", inject(function($componentController) {
-		var ctrl = $componentController("telefonbuchAnzeige");
-		expect(ctrl.Kontakte.length).toBe(24);
-	}));
-})
