2777b1dd98a23fc57f21dea85bb6e657599e0149
Martin <martin@example.com>
2017-05-01 14:35:31 +0200

    + einheitlichere Bennenung von Variablen + Fehler bei der Suche behoben

--- app/js/TelefonbuchAnzeige.controller.js
+++ app/js/TelefonbuchAnzeige.controller.js
@@ -2,18 +2,19 @@ angular.
 	module("telefonbuch").
 	controller("telefonbuchAnzeige", ['$scope', function ($scope) {
 			var Self = $scope;
-			// zu Testzwecken werden Einträge generiert.
+			
+// zu Testzwecken werden Einträge generiert.
 			Self.InitStoreFill = function (){
 				var Vornamen = ["Anja", "Bernhard", "Max", "Mara", "Thorsten", "Sabine"];
 				var Nachnamen = ["Schmidt", "Müller", "Mayer", "Leineweber"];
-				Self.Kontakte = Array(Vornamen.length * Nachnamen.length);
+				Self.Entries = Array(Vornamen.length * Nachnamen.length);
 				for (i = 0; i < Vornamen.length; i++) {
 					for (j = 0; j < Nachnamen.length; j++) {
 						var zahl = 37*(7**3+i)*(9**1+j);
 						var t = zahl % 65536;
 						var f = (3*zahl) % 65536;
 						var index = i*Nachnamen.length + j;
-						Self.Kontakte[index] = {
+						Self.Entries[index] = {
 							Index: index,
 							Vorname: Vornamen[i],
 							Nachname: Nachnamen[j],
@@ -30,9 +31,9 @@ angular.
 				Self.EditedIndex = -1;
 				Self.NewEntry = false;
 			};
-			// zu Testzwecken werden alle Einträge gelöscht.
+// zu Testzwecken werden alle Einträge gelöscht.
 			Self.InitStoreEmpty = function (){
-				Self.Kontakte = [];
+				Self.Entries = [];
 				Self.SaveLocalStorage();
 				Self.EditedEntry = {};
 				Self.ConfirmDelete = false;
@@ -40,9 +41,11 @@ angular.
 				Self.NewEntry = false;
 			};
 
-			Self.DoFilter = false;
-			Self.Filter = "";
-			Self.Kontakte = [];	// Kontakte hält die permanent gespeicherten Daten.
+			Self.Filter = {
+				Enabled: false,
+				Text: ""
+			};
+			Self.Entries = [];	// Entries hält die permanent gespeicherten Daten.
 			Self.EditedEntry = {};
 			Self.ConfirmDelete = false;
 			Self.EditedIndex = -1;
@@ -51,13 +54,13 @@ angular.
 // Es wird versucht die Einträge aus dem LocalStorage zu laden.
 			Self.StorageText = localStorage.getItem("Telefonbuch");
 			if (Self.StorageText) {
-				Self.Kontakte = JSON.parse(Self.StorageText);
+				Self.Entries = JSON.parse(Self.StorageText);
 			}
 
 			Self.SaveLocalStorage = function (todos) {
 // Die Funktion SaveLocalStorage wird verwendet um die Daten nach jedem Bearbeiten zu speichern.
-//				Self.StorageText = JSON.stringify(Self.Kontakte);
-				Self.StorageText = angular.toJson(Self.Kontakte);
+//				Self.StorageText = JSON.stringify(Self.Entries);
+				Self.StorageText = angular.toJson(Self.Entries);
 				localStorage.setItem("Telefonbuch", Self.StorageText);
 			};
 
@@ -73,8 +76,8 @@ angular.
 			Self.StartEdit = function (Index) {
 // Wird durch Buttons aufgerufen um die Bearbeitung eines Eintrags zu starten.
 // Wird bereits ein Eintrag bearbeitet oder angelegt geschieht nichts.
-				if (!Self.NewEntry && Self.EditedIndex == -1 && Index >= 0 && Index < Self.Kontakte.length) {
-					Self.CopyEntry(Self.EditedEntry, Self.Kontakte[Index]);
+				if (!Self.NewEntry && Self.EditedIndex == -1 && Index >= 0 && Index < Self.Entries.length) {
+					Self.CopyEntry(Self.EditedEntry, Self.Entries[Index]);
 					Self.EditedIndex = Index;
 					Self.NewEntry = false;
 					Self.ConfirmDelete = false;
@@ -89,7 +92,7 @@ angular.
 			Self.SaveEdit = function () {
 // Ist die Bearbeitenmaske für einen Eintrag offen werden die Änderungen übernommen und es wird gespeichert. Ansonsten passiert nichts.
 				if (Self.EditedIndex != -1) {
-					Self.CopyEntry(Self.Kontakte[Self.EditedIndex], Self.EditedEntry);
+					Self.CopyEntry(Self.Entries[Self.EditedIndex], Self.EditedEntry);
 					Self.SaveLocalStorage();
 					Self.EditedIndex = -1;
 				}
@@ -107,12 +110,12 @@ angular.
 					// Es muss sichergestellt werden, dass die Indizierung weiterhin korrekt ist.
 					// Da AngularJS die Sortierung übernimmt, genügt es den zu löschenden Eintrag
 					// durch den letzten Eintrag zu ersetzen, nur dessen Index zu aktualisieren
-					// und das Array Kontakte um eins zu verkürzen.
-					if (Self.EditedIndex < Self.Kontakte.length - 1) {
-						Self.Kontakte[Self.EditedIndex] = Self.Kontakte[Self.Kontakte.length - 1];
-						Self.Kontakte[Self.EditedIndex].Index = Self.EditedIndex;
+					// und das Array Entries um eins zu verkürzen.
+					if (Self.EditedIndex < Self.Entries.length - 1) {
+						Self.Entries[Self.EditedIndex] = Self.Entries[Self.Entries.length - 1];
+						Self.Entries[Self.EditedIndex].Index = Self.EditedIndex;
 					}
-					Self.Kontakte.splice(Self.Kontakte.length - 1);
+					Self.Entries.splice(Self.Entries.length - 1);
 					Self.EditedIndex = -1;
 					Self.SaveLocalStorage();
 				}
@@ -135,9 +138,9 @@ angular.
 // Ist die Bearbeitenmaske für einen neuen Eintrag offen wird ein neuer Eintrag erstellt und es wird gespeichert. Ansonsten passiert nichts.
 				if (Self.NewEntry) {
 					Self.NewEntry = false;
-					Self.Kontakte.push({});
-					Self.CopyEntry(Self.Kontakte[Self.Kontakte.length-1], Self.EditedEntry);
-					Self.Kontakte[Self.Kontakte.length-1].Index = Self.Kontakte.length-1;
+					Self.Entries.push({});
+					Self.CopyEntry(Self.Entries[Self.Entries.length-1], Self.EditedEntry);
+					Self.Entries[Self.Entries.length-1].Index = Self.Entries.length-1;
 					Self.SaveLocalStorage();
 				}
 			};
@@ -148,12 +151,16 @@ angular.
 				}
 			};
 			Self.StartFilter = function() {
-				Self.DoFilter = true;
-				Self.Filter = "";
+				Self.Filter = {
+					Enabled: true,
+					Text: ""
+				};
 			}
 			Self.CloseFilter = function() {
-				Self.DoFilter = false;
-				Self.Filter = "";
+				Self.Filter = {
+					Enabled: false,
+					Text: ""
+				};
 			}
 		}
 	]);
